f75751438763c153fa212e8a918b8f42f6701e01
Nicolasink3 <nicolas.r.p2018@gmail.com>
2025-10-06 10:14:22 -0300

    Add files via upload

--- Front/script.js
+++ Front/script.js
@@ -1,3 +1,40 @@
-const resultsDiv = document.getElementById("results")
+const resultsDiv = document.getElementById("results");
 
-console.log(resultsDiv)
+async function personagens() {
+  resultsDiv.innerHTML = "<p>Carregando...</p>";
+
+  try {
+    const response = await fetch('../Dados/data.json');
+    const data = await response.json();
+    console.log(data);
+
+    if (data.error) {
+      resultsDiv.innerHTML = "<p>Erro ao buscar personagens</p>";
+      return;
+    }
+    resultsDiv.innerHTML = '';
+
+    data.forEach(persona => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <img src="${persona.imagem}" alt="${persona.nome}">
+        <h2>${persona.nome}</h2>
+        <p class="descricao"><strong>Descrição:</strong> ${persona.descricao}</p>
+        <p class="aparencia"><strong>Aparência:</strong> ${persona.aparencia}</p>
+      `;
+
+      // Faz todo o card clicável
+      card.addEventListener('click', () => {
+        window.open(persona.wiki, '_blank');
+      });
+
+      resultsDiv.appendChild(card);
+    });
+  } catch (error) {
+    resultsDiv.innerHTML = "<p>Erro ao buscar personagens</p>";
+  }
+}
+
+personagens();
